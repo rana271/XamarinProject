0f382198a8442e29556ab1315f1e075becac08b8
rana271 <ranacomp@gmail.com>
2023-02-21 13:25:41 +0530

    Add project files.

---
+++ GitDemo/GitDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GitDemo.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GitDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
